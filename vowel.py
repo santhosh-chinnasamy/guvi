fcc850d383eafd7ecf476cb07c691a41ae18e2a5
Santhosh <csesanthosh15@gmail.com>
2018-05-31 10:41:55 +0530

    Add vowel program

---
+++ vowel.py
@@ -0,0 +1,5 @@
+str=raw_input('enter a string to check whether it is a vowel or not:')
+if (str=="a" or str=="e" or str=="i" or str=="o" or str=="u"):
+    print('it has vowel')
+else:
+    print('it is consonant')
